bc69bf8c9b4f46826f43b35e0ac9f169dfae936e
Marco Túlio Teixeira <tulioteixeira@google.com>
2018-07-16 00:39:29 -0300

    Removing unused import.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
